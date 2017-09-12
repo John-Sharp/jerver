34fda36f60111c3db9d1de6e3952d8d10c2947ff
John Sharp <johnjonnosharp@gmail.com>
2017-09-13 00:04:11 +0100

    added REST API methods for users, including making compatible with CORS request

--- main.go
+++ main.go
@@ -3,69 +3,141 @@ package main
 import (
     "net/http"
     "fmt"
-    //"log"
     "encoding/json"
-    // "bytes"
-    // "net/url"
+    "github.com/satori/go.uuid"
 )
 
 type user struct {
+    Uuid uuid.UUID
     FirstName string
     SecondName string
-    Password string
 }
 
 var users []user
 
-func userHandler(w http.ResponseWriter, r *http.Request) {
+func init() {
+    users = []user{}
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
     case "GET":
         var ej []byte
         ej , err := json.Marshal(users)
         if err != nil {
-            w.WriteHeader(http.StatusInternalServerError)
+            http.Error(w, "error decoding JSON", http.StatusInternalServerError)
             return
         }
 
+        w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8090")
         fmt.Fprint(w, string(ej))
         return
 
     case "POST":
-        err := r.ParseForm()
-        if err != nil {
-            w.WriteHeader(http.StatusBadRequest)
+        w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8090")
+        firstName := r.PostFormValue("firstName")
+        secondName := r.PostFormValue("secondName")
+
+        if firstName == "" || secondName == "" {
+            http.Error(w, "error parsing form", http.StatusInternalServerError)
             return
         }
 
-        if val, ok := r.PostForm["firstName"]; ok {
-            if len(val) != 1 {
-                w.WriteHeader(http.StatusBadRequest)
-                return
-            }
-        }
-        if val, ok := r.PostForm["secondName"]; ok {
-            if len(val) != 1 {
-                w.WriteHeader(http.StatusBadRequest)
-                return
-            }
-        }
-        if val, ok := r.PostForm["password"]; ok {
-            if len(val) != 1 {
-                w.WriteHeader(http.StatusBadRequest)
-                return
+        users = append(users, user{
+            uuid.NewV4(),
+            r.PostForm["firstName"][0],
+            r.PostForm["secondName"][0]})
+    default:
+    }
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+    userUuid, err := uuid.FromString(r.URL.Path)
+    if err != nil {
+        http.Error(w, "error decoding UUID", http.StatusInternalServerError)
+        return
+    }
+
+    switch r.Method {
+    case "OPTIONS":
+        w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8090")
+        w.Header().Add("Access-Control-Allow-Methods", "GET, PUT, DELETE")
+        return
+    case "PUT":
+        w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8090")
+        found := false
+        for i, u := range users {
+            if uuid.Equal(u.Uuid, userUuid) {
+                found = true
+
+                if r.PostFormValue("firstName") != "" {
+                    users[i].FirstName = r.PostFormValue("firstName")
+                }
+                if r.PostFormValue("secondName") != "" {
+                    users[i].SecondName = r.PostFormValue("secondName")
+                }
+                break
             }
         }
 
-        users = append(users, user{
-            r.PostForm["firstName"][0],
-            r.PostForm["secondName"][0],
-            r.PostForm["password"][0]})
+        if !found {
+            http.Error(w, "user not found", http.StatusInternalServerError)
+            return
+        }
+
+        return
+    case "DELETE":
+        w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8090")
+        found := false
+        var i int
+        var u user
+        for i, u = range users {
+            if uuid.Equal(u.Uuid, userUuid) {
+                found = true
+            }
+            break
+        }
+
+        if !found {
+            http.Error(w, "user not found", http.StatusInternalServerError)
+            return
+        }
+
+        users = append(users[:i], users[i+1:]...)
+    case "GET":
+        w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8090")
+        found := false
+
+        var i int
+        var u user
+        for i, u = range users {
+            if uuid.Equal(u.Uuid, userUuid) {
+                found = true
+            }
+            break
+        }
+
+        if !found {
+            http.Error(w, "user not found", http.StatusInternalServerError)
+            return
+        }
+
+        var ej []byte
+        ej , err := json.Marshal(users[i])
+        if err != nil {
+            http.Error(w, "error decoding JSON", http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8090")
+        fmt.Fprint(w, string(ej))
     default:
     }
 }
 
 func main() {
-    http.HandleFunc("/users", userHandler)
+    http.HandleFunc("/users", usersHandler)
+    http.Handle("/users/", http.StripPrefix("/users/", http.HandlerFunc(userHandler)))
 
     http.ListenAndServe(":8080", nil)
 }
